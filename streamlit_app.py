4c2a0adb5fcd40fb6f93269eba3c0ce568034116
emilykblake <126585748+emilykblake@users.noreply.github.com>
2023-04-04 12:10:21 +0100

    Update streamlit_app.py
    formatting

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-my_fruit_list = pd.read_csv(r"https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.dataframe(my_fruit_list)
-
 st.title('New Diner Menu')
+
 st.header('Br-egg-fast')
 st.text('🐔 Eggs Benedict')
 st.text('🐔 Eggs Florentine')
@@ -14,4 +12,5 @@ st.header('Other Normal Breakfast Things')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+my_fruit_list = pd.read_csv(r"https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
