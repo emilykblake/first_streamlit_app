0f0468f92f475f257f68f9bf8894b29506da8b30
emilykblake <126585748+emilykblake@users.noreply.github.com>
2023-04-04 11:59:06 +0100

    Update streamlit_app.py
    added avo toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
 st.title('New Diner Menu')
-st.header('Br-egg-fast')
+st.header('Br-egg-fast🐔')
 st.text('Eggs Benedict')
 st.text('Eggs Florentine')
 st.text('Eggs Royale')
+st.header('Other Normal Breakfast Things')
+st.text('Avocado Toast🥑🍞')
