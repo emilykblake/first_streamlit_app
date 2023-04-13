6b23ab63a858e106c15cadb606bfdc1b07cd75ba
emilykblake <126585748+emilykblake@users.noreply.github.com>
2023-04-13 16:13:32 +0100

    Update streamlit_app.py
    try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,18 @@ st.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-# take the json version of the response and normalize
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# output it on the screen as a table
-st.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # take the json version of the response and normalize
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # output it on the screen as a table
+    st.dataframe(fruityvice_normalized)
+exceptURLError as e:
+  st.error()
 
 # dont run anything past here while we troubleshoot
 st.stop()
